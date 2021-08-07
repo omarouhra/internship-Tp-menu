685b50a38dddac8fae7761dc9232fc1dd01dc26d
Omar Ouhra <omarouhra@Omars-Mac-mini.local>
2021-08-07 19:10:40 +0100

    scrit

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 const link = document.querySelector(".link");
 const back = document.querySelector(".back");
 const menu = document.querySelector(".menu");
+const body = document.body;
+const focusableSelector = "button,a,input";
+let focusables = Array.from(menu.querySelectorAll(focusableSelector));
 
 link.addEventListener("click", (e) => {
   e.preventDefault();
   menu.style.display = "block";
+  menu.style.animation = "fade-in .2s ease-in";
+  body.style.backgroundColor = "grey";
+
+  menu.removeAttribute("aria-hidden");
+  menu.setAttribute("aria-modal", "true");
+
+  console.log(focusables);
 });
+
+// focus
+
+const focusInMenu = (e) => {
+  e.preventDefault();
+  let index = focusables.findIndex((f) => f === menu.querySelector(":focus"));
+  index++;
+
+  if (index >= focusables.length) {
+    index=0
+  }
+  focusables[index].focus()
+
+};
+
+// close menu function
+const closeMenu = () => {
+  menu.style.animation = "fade-out .2s ease-in";
+  body.style.backgroundColor = "whitesmoke";
+  menu.removeAttribute("aria-modal");
+  menu.setAttribute("aria-hidden", "true");
+
+  setTimeout(() => {
+    menu.style.display = "none";
+  }, 200);
+};
 
 back.addEventListener("click", (e) => {
   e.preventDefault();
-  menu.style.display = "none";
+  closeMenu();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeMenu();
+  }
+  if (e.key === "Tab" && menu !== null) {
+    focusInMenu(e);
+  }
 });
